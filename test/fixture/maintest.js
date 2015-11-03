940fe24e936df88799c5a7815724eaa7880e5ac4
typhonrt <support@typhonrt.org>
2015-11-03 15:35:54 -0800

    Initial commit

--- test/fixture/maintest.js
+++ test/fixture/maintest.js
@@ -2,5 +2,4 @@
  * Basic Build testing. The below imports simply link all source in this repo for an in memory bundle op test.
  */
 import '../../src/collections/AllParseUsers.js';
-import '../../src/lifecycle/ParseInitialize.js';
 import '../../src/models/ParseUser.js';
