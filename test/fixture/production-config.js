0848500f39a2583433fd8e27925a99a82952f930
typhonrt <support@typhonrt.org>
2015-11-03 15:38:38 -0800

    Initial commit

--- test/fixture/production-config.js
+++
@@ -1,9 +0,0 @@
-/**
- * production-config.js -- Defines the production keys for Parse.
- */
-'use strict';
-
-export default {
-   parseAppID: 'THIS IS A TEST APP ID',
-   parseAPIKey: 'THIS IS A TEST API KEY'
-};
